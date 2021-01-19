45a2a56aa33d06db24c65885fcbd86cee3c8d3a6
Zhusong <song.zhu@lasong.com.cn>
2021-01-19 17:21:05 +0800

    更新爬虫公告地址

--- CarCrawler.py
+++ CarCrawler.py
@@ -22,7 +22,7 @@ from email.header import Header
 import sys
 
 keyword = "阶梯"
-url = "http://xkctk.hangzhou.gov.cn/tzgg/"
+url = "https://hzxkctk.cn/tzgg/"
 User_Agent = " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
 headers = {'User-Agent': User_Agent}
 
